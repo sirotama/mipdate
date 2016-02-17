25a4152685ca97df6db00adeb890fddcc8ca4e21
sirotama <magicsirotama@gmail.com>
2016-02-18 08:14:04 +0900

    #fuckcsrf

--- main.py
+++ main.py
@@ -2,25 +2,35 @@ import bottle
 from bottle_jade import JadePlugin
 import requests
 import json
+import sys
 import linecache
 
 VIEWS_DIR=linecache.getline('id',3).rstrip()
 
 jade=bottle.install(JadePlugin(template_folder=VIEWS_DIR))
+
+ids = linecache.getline('id',1).rstrip()
+password = linecache.getline('id',2).rstrip()
+
+login = requests.post('https://login.misskey.xyz' ,data={'screen-name': ids, 'password': password})
+cookie = {'hmsk': login.cookies['hmsk']}
+
+updateAPIURL = 'https://himasaku.misskey.xyz/account/name/update'
+
 def render(jadename,jadeopts={}):
 	return jade.render(jadename,**jadeopts)
 
 @bottle.get('/')
 def index():
 
-	ids = linecache.getline('id',1).rstrip()
-	password = linecache.getline('id',2).rstrip()
-
-	login = requests.post('https://login.misskey.xyz' ,data={'screen-name': ids, 'password': password})
-	cookie = {'hmsk': login.cookies['hmsk']}
 
 	return render("index.jade")
 @bottle.post('/change')
 def changename():
-	return render("change.jade")
+		changedname = bottle.request.forms.get("name")
+		change = requests.post(updateAPIURL,cookies=cookie,data=changedname)
+		print changedname
+		print change.text
+		return render("change.jade")
+
 bottle.run(host='localhost', port=8080, debug=True, reloader=True)
