b5f9b8ad60ac97c88f9506227717f0ac52d17f67
sirotama <magicsirotama@gmail.com>
2016-02-18 09:27:01 +0900

    PERFECT

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import bottle
 from bottle_jade import JadePlugin
-import requests
+import requests,re
 import json
 import sys
 import linecache
@@ -12,10 +12,14 @@ jade=bottle.install(JadePlugin(template_folder=VIEWS_DIR))
 ids = linecache.getline('id',1).rstrip()
 password = linecache.getline('id',2).rstrip()
 
-login = requests.post('https://login.misskey.xyz' ,data={'screen-name': ids, 'password': password})
-cookie = {'hmsk': login.cookies['hmsk']}
-
 updateAPIURL = 'https://himasaku.misskey.xyz/account/name/update'
+postAPIURL= "https://himasaku.misskey.xyz/posts/create"
+res = requests.get("https://misskey.xyz/")
+res.raise_for_status()
+cookie = {"hmsk":res.cookies["hmsk"]}
+csrf = re.search('<meta name="csrf-token" content="([A-Za-z0-9\\-_]+)">',res.text).group(1)
+res = requests.post("https://login.misskey.xyz/",headers={'csrf-token':csrf},data={"screen-name":ids,"password":password},cookies = cookie)
+res.raise_for_status()
 
 def render(jadename,jadeopts={}):
 	return jade.render(jadename,**jadeopts)
@@ -27,10 +31,9 @@ def index():
 	return render("index.jade")
 @bottle.post('/change')
 def changename():
-		changedname = bottle.request.forms.get("name")
-		change = requests.post(updateAPIURL,cookies=cookie,data=changedname)
-		print changedname
-		print change.text
+		changedname = {'name': bottle.request.forms.get("name")}
+		change = requests.post(updateAPIURL,cookies=cookie,data=changedname,headers={"csrf-token":csrf} )
+
 		return render("change.jade")
 
 bottle.run(host='localhost', port=8080, debug=True, reloader=True)
